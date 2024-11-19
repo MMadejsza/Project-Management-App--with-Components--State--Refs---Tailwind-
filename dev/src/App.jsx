2ad4b2ae9083de3995fb8c4df2c5a3f863e452e0
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-11-19 16:32:30 +0000

    neProject and input components

--- dev/src/App.jsx
+++ dev/src/App.jsx
@@ -1,10 +1,13 @@
 import SideBar from './components/sideBar.jsx';
+import NewProject from './components/NewProject.jsx';
+// import NewProject from './components/NewProject.jsx';
 
 function App() {
 	return (
 		<>
-			<main className='h-screen my-8'>
+			<main className='h-screen my-8 flex gap-8'>
 				<SideBar />
+				<NewProject />
 			</main>
 		</>
 	);
